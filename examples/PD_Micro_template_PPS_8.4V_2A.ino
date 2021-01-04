a2fe74882dc23acd352c3cf8da27d53cffaa52ca
Ryan Ma <ryan123ma@hotmail.com>
2021-01-05 01:43:08 +0800

    Add example templates

---
+++ examples/PD_Micro_template_PPS_8.4V_2A.ino
@@ -0,0 +1,21 @@
+
+#include <Wire.h>
+#include "src/PD_UFP.h"
+
+class PD_UFP_c PD_UFP;
+
+void setup() {
+  Wire.begin();
+  PD_UFP.init_PPS(PPS_V(8.4), PPS_A(2.0));
+}
+
+void loop() {
+  PD_UFP.run();
+  if (PD_UFP.is_PPS_ready()) {          // PPS trigger success
+    PD_UFP.set_output(1);               // Turn on load switch 
+    PD_UFP.set_led(1);                  // PPS output 4.2V 2.0A ready
+  } else if (PD_UFP.is_power_ready()) { // Fail to trigger PPS, fall back
+    PD_UFP.set_output(0);               // Turn off load switch
+    PD_UFP.blink_led(400);              // blink LED
+  }
+}
